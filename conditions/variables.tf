73ad97ae6c7335035d44e9f1b885df04b8eb772a
venu <venubonkuri1234@gmail.com>
2025-07-10 11:03:37 +0530

    terraform

---
+++ conditions/variables.tf
@@ -0,0 +1,49 @@
+variable "ami_id" {
+    type        = string
+    default     = "ami-09c813fb71547fc4f"
+    description = "ami id of joindevops RHEL9"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name        = "hello world"
+        purpose = "variables-demo"
+    }
+}
+
+variable "sg_name" {
+    default = "vars-file-allow-all"
+}
+
+variable "sg_description" {
+    default = "Allowing all ports from internet"
+}
+
+variable "from_port" {
+    default = 0
+}
+
+variable "to_port" {
+    type   = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+
+variable "environment" {
+    default = "dev" # Change this to "prod" to test the condition 
+}
---
+++ ec2/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "roboshop" {
+  ami        = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name        = "hello world"   
+  }
+    
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+  description = "allowing all ports from internet" 
+
+  ingress {
+
+     from_port        = 0
+     to_port          = 0
+     protocol         = "-1"
+     cidr_blocks      = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+   
+  }
+  egress {
+     
+     from_port        = 0
+     to_port          = 0    #Using "-1" for protocol allows all protocols
+     protocol         = "-1"
+     cidr_blocks      = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+
+  }
+  
+  tags =  {
+    Name = "allow-all"
+  }
+
+}
